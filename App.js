d8603c032fe83e7e04b61c964452121e50af5ada
MeLikeApplez <jeremisibayan@gmail.com>
2023-04-17 18:15:18 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const server = app.listen(8080, () => console.log('Listening...'))
+
+require('dotenv').config()
+
+const CORS_ORIGIN = '*'
+const cors = require('cors')
+const io = require('socket.io')(server, { cors: { origin: CORS_ORIGIN } })
+
+app.use(cors({ origin: CORS_ORIGIN }))
+
+io.on('connection', socket => {
+    console.log(`Socket Connected: ${socket.id}`)
+})
+
+async function youtubeKeywordSearch(keywords) {
+    try {
+        const search = await YoutubeSearchApi.GetListByKeyword(keywords, false, 1)
+        const { items } = search
+        
+        // empty results
+        if(items.length <= 0) return { data: null, error: 'Not Found' }
+
+        return{ data:  items[0] ,error: null }
+
+    } catch(err) {
+        return { data: null, error: JSON.stringify(err) }
+    }
+}
