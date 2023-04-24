ef6e476c4ba7387568b617957190c897b00fe268
MeLikeApplez <jeremisibayan@gmail.com>
2023-04-23 20:53:29 -0500

    Route handling & Cache.json attempt

--- App.js
+++ App.js
@@ -5,6 +5,9 @@ const server = app.listen(8080, () => console.log('Listening...'))
 require('dotenv').config()
 
 const { PassThrough } = require('stream')
+const fs = require('fs')
+const path = require('path')
+
 const { v4: uuidv4 } = require('uuid')
 const CORS_ORIGIN = '*'
 const cors = require('cors')
@@ -12,14 +15,34 @@ const cors = require('cors')
 const YoutubeSearchApi = require('youtube-search-api')
 const ytdl = require('ytdl-core')
 
-// const { MongoClient } = require('mongodb')
-// const mongo = new MongoClient(process.env.MONGO_URL)
-// const _mongoConnect = mongo.connect()
-
 app.use(cors({ origin: CORS_ORIGIN }))
-// app.use('/', (req, res) => res.sendStatus(200))
+app.use((req, res, next) => {
+    if(req.path !== '/api' && req.path !== '/api/count') return res.sendStatus(200)
+    
+    next()
+})
 
 const bytesToMegaBytes = buf => Buffer.byteLength(buf) / 1e+6
+const formatMemoryUsage = data => Math.round(data / 1024 / 1024 * 100) / 100
+
+const memoryData = process.memoryUsage()
+const memoryUsage = formatMemoryUsage(memoryData.rss + memoryData.heapTotal + memoryData.heapUsed + memoryData.external + memoryData.arrayBuffers)
+
+console.log(
+    `\x1b[34mMemory: ${memoryUsage} MB\x1b[37m`
+)
+
+app.get('/api/count', (req ,res) => {
+    const getJSON = () => JSON.parse(fs.readFileSync(path.join(__dirname, 'Cache.json'), 'utf-8'))
+
+    fs.writeFileSync(path.join(__dirname, 'Cache.json'), JSON.stringify({
+        ...getJSON(),
+        count: getJSON()?.count + 1 ?? 0,
+        lastUpdated: new Date()
+    }))
+
+    res.json(getJSON())
+})
 
 app.get('/api',async (req, res) => {
     if(!('keywords' in req.query)) return res.sendStatus(400)
@@ -31,13 +54,10 @@ app.get('/api',async (req, res) => {
     try {
         // https://stackoverflow.com/questions/19553837/node-js-piping-the-same-readable-stream-into-multiple-writable-targets
         const download = ytdl(url, { filter: format => format.hasVideo && format.hasAudio })
-        // const cacheDownload = ytdl(url, { filter: format => format.hasVideo && format.hasAudio })
         // const info = await ytdl.getInfo(searchResult.data.id)
         // const formats = info.formats.find(format => format.hasVideo && format.hasAudio)
         // const { codecs, videoCodec, audioCodec } = formats
         
-        // const cachePassThrough = new PassThrough()
-
         res.setHeader('Content-Type', 'audio/mp4')
 
         // CacheAudio({ stream: cacheDownload, url, keywords: req.query.keywords })
@@ -52,47 +72,7 @@ app.get('/api',async (req, res) => {
 async function CacheAudio({ stream, url='', keywords='' }={}) {
     if(!(stream instanceof PassThrough)) return console.error('Failed to cache, Stream invalid', stream)
 
-    console.time('mongo')
-    await mongo.connect()
-
-    console.log('[+] Cache Connected')
-    const cache = mongo.db('Database').collection('Cache')
-    const expireTime = 5
-    const item = await cache.findOne({ keywords })
-
-    console.log(!!item)
-
-    if(item === null) {
-        try {
-            const chunks = []
-            const buffer = await new Promise((resolve, reject) => {
-                stream.on('data', chunk => chunks.push(chunk))
-                stream.on('end', () => resolve(Buffer.concat(chunks)))
-                stream.on('error', err => reject(err))
-            })
-
-            await cache.createIndex({ expiresAt: 1 }, { expireAfterSeconds: 0 })
-            await cache.insertOne({
-                url, keywords,
-                uuid: uuidv4(),
-                buffer: buffer,
-                megabyte: bytesToMegaBytes(buffer),
-                bytes: Buffer.byteLength(buffer),
-                expiresAt: new Date(Date.now() + expireTime * 60 * 1000)
-            })
-        } catch(err) {
-            console.error(err)
-        }
-    } else {
-        const binary = item.buffer.buffer//.match(/"(.*?)"/g)?.[0]
-
-        // console.log(binary)
-    }
-
-    console.timeEnd('mongo')
-    mongo.close()
-
-    console.log('[-] Cache Closed')
+    
 }
 
 async function youtubeKeywordSearch(keywords) {
